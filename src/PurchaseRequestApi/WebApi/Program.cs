67fabde47ccd407827aad5e8fcc57fa12c58a439
Aliaksei Banshchyk <alexban04@gmail.com>
2026-04-28 14:59:12 +0300

    fixed logger

--- src/PurchaseRequestApi/WebApi/Program.cs
+++ src/PurchaseRequestApi/WebApi/Program.cs
@@ -4,16 +4,16 @@ using WebApi;
 using AutoWrapper;
 using Serilog;
 
+var builder = WebApplication.CreateBuilder(args);
+
 using var log = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
     .WriteTo.Console()
     .CreateLogger();
     
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+builder.Host.UseSerilog(log);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
@@ -22,18 +22,17 @@ builder.Services.AddDb();
 builder.Services.AddMediatr();
 
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseApiResponseAndExceptionWrapper();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
