d9011d50e339b484980a2737470f3d045ab1839f
Aliaksei Banshchyk <alexban04@gmail.com>
2026-04-29 22:19:34 +0300

    migrations changes

---
+++ src/PurchaseRequestApi/Infrastructure/Migrations/20260429191837_RequestUpdate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RequestUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Requests",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Requests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EditedAt",
+                table: "Requests",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Requests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "EditedAt",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Requests");
+        }
+    }
+}
