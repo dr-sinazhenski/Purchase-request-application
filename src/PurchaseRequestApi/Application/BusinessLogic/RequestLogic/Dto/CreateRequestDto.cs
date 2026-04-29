fc0dd9169e039c4092b93f92da26fe315075cdd9
Aliaksei Banshchyk <alexban04@gmail.com>
2026-04-30 02:39:09 +0300

    just a minor change to make get all requests work

--- src/PurchaseRequestApi/Application/BusinessLogic/RequestLogic/Dto/CreateRequestDto.cs
+++ src/PurchaseRequestApi/Application/BusinessLogic/RequestLogic/Dto/CreateRequestDto.cs
@@ -6,6 +6,7 @@ namespace Application.BusinessLogic.RequestLogic.Dto
 {
     public class CreateRequestDto
     {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public Guid requestTypeId { get; set; }
--- src/PurchaseRequestApi/WebApi/Controllers/RequestController.cs
+++ src/PurchaseRequestApi/WebApi/Controllers/RequestController.cs
@@ -1,5 +1,6 @@
 using Application.BusinessLogic.RequestLogic.CreateRequest;
 using Application.BusinessLogic.RequestLogic.Dto;
+using Application.BusinessLogic.RequestLogic.GetAllRequests;
 using Application.BusinessLogic.RequestTypeLogic.Dto;
 using Application.BusinessLogic.RequestTypeLogic.GetAllRequestTypes;
 using MediatR;
@@ -28,6 +29,20 @@ namespace WebApi.Controllers
             if (!result.IsSuccess)
             {
                 _logger.LogError("Failed to create request");
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllRequestsCommand());
+
+            if (!result.IsSuccess)
+            {
+                _logger.LogError("Failed to fetch requests");
                 return BadRequest(result);
             }
 
