f56c6ce7ef8686c206d6864edc84fa412870f02f
Aliaksei Banshchyk <alexban04@gmail.com>
2026-05-19 00:44:57 +0300

    added request filtering

---
+++ src/PurchaseRequestApi/Application/BusinessLogic/RequestLogic/Dto/GetRequestsFilteredDto.cs
@@ -0,0 +1,13 @@
+namespace Application.BusinessLogic.RequestLogic.Dto
+{
+    public class GetRequestsFilteredDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string RequestType { get; set; }
+        public string Status { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
