812540a55daa193d36d385a69f66a2022089f901
OdintsovMikhail <mihail.odincov@student.ehu.lt>
2026-05-26 03:27:33 +0300

    Basic setup of integration tests using db in docker container

---
+++ src/PurchaseRequestApi/Testing/RequestTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.BusinessLogic.RequestLogic.CreateRequest;
+using Application.BusinessLogic.RequestLogic.Dto;
+using Application.BusinessLogic.RequestLogic.GetAllRequests;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Testing;
+
+[TestFixture]
+public class RequestTests : HandlerTestBase
+{
+    protected override async Task SeedAsync(AppDbContext db)
+    {
+        DBSeeder.SeedRequestType(db, name: "Bug Report");
+    }
+
+    [Test]
+    public async Task Add_adds_request_successfully()
+    {
+
+        var type = await Database.RequestTypes.FirstOrDefaultAsync();
+        var dto = new CreateRequestDto
+        {
+            Title = "123",
+            Description = "456",
+            RequestTypeId = type.Id,
+        };
+
+        var responce = await Mediator.Send(new CreateRequestCommand(dto));
+
+        Assert.That(responce.IsSuccess, Is.EqualTo(true));
+
+        var count = await Database.Requests.CountAsync(c => c.Title == dto.Title);
+        Assert.That(count, Is.EqualTo(1));
+    }
+}
---
+++ src/PurchaseRequestApi/Testing/Tests.cs
@@ -0,0 +1,63 @@
+﻿using DotNet.Testcontainers.Builders;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using NUnit.Framework;
+using Respawn;
+using Testcontainers.PostgreSql;
+
+[SetUpFixture]
+public class Tests
+{
+    private static readonly PostgreSqlContainer DatabaseContainer = new PostgreSqlBuilder()
+      .WithImage("postgres:latest")
+      .WithDatabase("testdb")
+      .WithUsername("testuser")
+      .WithPassword("testpassword")
+      .WithWaitStrategy(Wait
+        .ForUnixContainer()
+        .UntilMessageIsLogged("database system is ready to accept connections"))
+      .Build();
+
+    private static Respawner respawner = default!;
+
+    private static RespawnerOptions respawnerOptions = new() { DbAdapter = DbAdapter.Postgres };
+
+    public static string ConnectionString => DatabaseContainer.GetConnectionString();
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        await DatabaseContainer.StartAsync();
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+          .UseNpgsql(ConnectionString)
+          .Options;
+
+        using var db = new AppDbContext(options);
+
+        await db.Database.EnsureCreatedAsync();
+
+        await using var connection = new NpgsqlConnection(ConnectionString);
+
+        await connection.OpenAsync();
+
+        respawner = await Respawner.CreateAsync(connection, respawnerOptions);
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await DatabaseContainer.StopAsync();
+        await DatabaseContainer.DisposeAsync();
+    }
+
+    public static async Task ResetDatabase()
+    {
+        await using var connection = new NpgsqlConnection(ConnectionString);
+
+        await connection.OpenAsync();
+
+        await respawner.ResetAsync(connection);
+    }
+}
