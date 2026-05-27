fcafd05a9cad558d431ea781826793d94a7e6c80
OdintsovMikhail <mihail.odincov@student.ehu.lt>
2026-05-27 06:15:59 +0300

    enabled db migrations and swagger on prod

--- src/PurchaseRequestApi/WebApi/Program.cs
+++ src/PurchaseRequestApi/WebApi/Program.cs
@@ -31,12 +31,9 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
